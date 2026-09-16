9d2c7344be0c577c25f688cba4db332695f11489
JTK <koolyestor@gmail.com>
2026-09-16 22:49:47 +0530

    Configure deployed chart stream endpoint

--- quant-radar/app/charts/page.tsx
+++ quant-radar/app/charts/page.tsx
@@ -3,5 +3,5 @@ import ChartsClient from "./ChartsClient";
 export const dynamic = "force-dynamic";
 
 export default function ChartsPage() {
-  return <ChartsClient streamUrl={process.env.NEXT_PUBLIC_CHART_STREAM_URL ?? ""} />;
+  return <ChartsClient streamUrl={process.env.NEXT_PUBLIC_CHART_STREAM_URL ?? "wss://140.238.241.210.sslip.io"} />;
 }
